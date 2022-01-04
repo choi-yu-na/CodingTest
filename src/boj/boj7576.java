ed1f8e5c7dd746cb90dd42562dca82b5e402e853
choiyuna <choiyuna0209@gmail.com>
2022-01-04 23:38:45 +0900

    boj7576 4th try 백준에 제출했는데 시간초과 뜬 것

--- src/boj/boj7576.java
+++ src/boj/boj7576.java
@@ -1,66 +1,77 @@
-//https://www.acmicpc.net/problem/7576
-//토마토
 package boj;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
-
 public class boj7576 {
     static int n, m;
     static int[][] storage;
-    static int tomato;
     static boolean[][] visited;
     static Queue<location> queue;
     static int[] dx = {1,-1,0,0};
     static int[] dy = {0,0,1,-1};
-    static int max;
-
+    static int day;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
+        String str = br.readLine();
+        m = Integer.parseInt(str.split(" ")[0]);
+        n = Integer.parseInt(str.split(" ")[1]);
         storage = new int[n][m];
-        queue = new LinkedList<>();
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
+            str = br.readLine();
             for (int j = 0; j < m; j++) {
-                storage[i][j] = Integer.parseInt(st.nextToken());
-                if(storage[i][j]==1) queue.offer(new location(i,j,0));
-                else if(storage[i][j]==0) tomato += 1;
+                storage[i][j] = Integer.parseInt(str.split(" ")[j]);
             }
         }
 
-        ripe();
-
-        System.out.println(tomato>0?-1:max);
-    }
-
-    public static void ripe() {
-        visited = new boolean[n][m];
-        while(!queue.isEmpty()) {
-            location now = queue.poll();
-            if(max < now.day) max = now.day;
-            for (int i = 0; i < 4; i++) {
-                int newX = now.x + dx[i];
-                int newY = now.y + dy[i];
-
-                if (0 > newX || newX >= n || 0 > newY || newY >= m) continue;
-                if (storage[newX][newY] == -1) continue;
-                if (visited[newX][newY]) continue;
-
-                queue.offer(new location(newX,newY, now.day+1));
-                storage[newX][newY]=1;
-                visited[newX][newY]=true;
-                tomato -= 1;
+        day=1;
+        queue = new LinkedList<>();
+        while(!isDone(storage)) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if(storage[i][j]==day && visited[i][j]==false) {
+                        visited[i][j]=true;
+                        queue.offer(new location(i,j));
+                        ripe(i,j);
+                    }
+                }
             }
             print(storage);
-
+            day++;
+            if(day>=(n*m)) {
+                System.out.println("-1");
+                return;
+            }
         }
+        System.out.println(day-1);
+
+    }
+
+    public static void ripe(int x, int y) {
+        location l = queue.poll();
+        for (int i = 0; i < 4; i++) {
+            int newX = l.x+dx[i];
+            int newY = l.y+dy[i];
+            if(-1<newX && newX<n && -1<newY && newY<m) {
+                if(storage[newX][newY]==0 && visited[newX][newY]==false) {
+                    storage[newX][newY]=day+1;
+                    //visited[newX][newY]=true;
+                }
+            }
+        }
+
+    }
+
+    public static boolean isDone(int[][] arr) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(arr[i][j]==0)
+                    return false;
+            }
+        }
+        return true;
     }
 
     public static void print(int[][] arr) {
@@ -76,12 +87,10 @@ public class boj7576 {
     public static class location {
         int x;
         int y;
-        int day;
 
-        public location(int x, int y, int day) {
+        public location(int x, int y) {
             this.x = x;
             this.y = y;
-            this.day = day;
         }
     }
 }
